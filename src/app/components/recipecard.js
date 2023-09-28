2b2da811d8f1ca9ea141975c7f87c04c13da6d66
Andre Pautin <63878264+andrepautin@users.noreply.github.com>
2023-09-27 18:35:18 -0700

    add more images for meal type and types add scrolling for recipe name overflow on dashboard

--- src/app/components/recipecard.js
+++ src/app/components/recipecard.js
@@ -1,25 +1,41 @@
-import { Card, CardContent, Paper } from "@mui/material";
+import { Card, CardContent, Paper, Tooltip, Typography } from "@mui/material";
 import Link from "next/link";
 import { recipeImages } from "../images/recipeimages";
 
 export default function RecipeCard({ recipe }) {
   return (
     <Link href={`/recipe/${recipe?.id}`}>
-      <Paper
-        elevation={24}
-        sx={{
-          height: 120,
-          width: 120,
-          m: 1,
-          // bgcolor: "#E9D0AA",
-          // add hover opacity/transparency
-          ":hover": { backgroundColor: "#9E691A" },
-          backgroundImage: `url('${recipeImages.any}')`,
-          backgroundSize: "cover",
-        }}
-      >
-        <CardContent>{recipe?.name}</CardContent>
-      </Paper>
+      <Tooltip title={recipe?.name}>
+        <Typography
+          sx={{
+            maxWidth: 120,
+            whiteSpace: "nowrap",
+            overflow: "auto",
+            scrollbarWidth: "none",
+            "&::-webkit-scrollbar": { display: "none" },
+            "&-ms-overflow-style": { display: "none" },
+            ml: 1,
+          }}
+        >
+          {recipe?.name}
+        </Typography>
+        <Paper
+          elevation={24}
+          sx={{
+            height: 120,
+            width: 120,
+            m: 1,
+            mt: 0,
+            ":hover": { opacity: 0.5 },
+            backgroundImage: `url('${
+              recipe?.mealType === "any"
+                ? recipeImages?.any
+                : recipeImages[recipe?.mealType][recipe?.type]
+            }')`,
+            backgroundSize: "cover",
+          }}
+        ></Paper>
+      </Tooltip>
     </Link>
   );
 }
--- src/app/images/recipeimages.js
+++ src/app/images/recipeimages.js
@@ -1,8 +1,25 @@
 export const recipeImages = {
   // could nest this by mealType
   // i.e. recipeImages = {breakfast: {food: '...', drink: '...'}}
-  food: "https://www.eatingwell.com/thmb/m5xUzIOmhWSoXZnY-oZcO9SdArQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/article_291139_the-top-10-healthiest-foods-for-kids_-02-4b745e57928c4786a61b47d8ba920058.jpg",
+  breakfast: {
+    food: "https://www.licious.in/blog/wp-content/uploads/2022/11/shutterstock_621756830-750x563.jpg",
+    drink:
+      "https://www.eatingbirdfood.com/wp-content/uploads/2023/01/Breafkast-Drinks-BLOG-IMAGE-min.jpg",
+  },
+  lunch: {
+    food: "https://www.yummytoddlerfood.com/wp-content/uploads/2022/04/Lunches-for-Work-1-horiz.jpg",
+    drink:
+      "https://www.acouplecooks.com/wp-content/uploads/2021/06/Strawberry-Water-006.jpg",
+  },
+  dinner: {
+    food: "https://www.spoonforkbacon.com/wp-content/uploads/2021/02/dinner-recipes.jpg",
+    drink:
+      "https://uploads-ssl.webflow.com/5d6afb6fe54b1ed29661308a/61257032403b2c5ead64b452_after%20dinner%20drinks.jpg",
+  },
+  snack: {
+    food: "https://www.yummytoddlerfood.com/wp-content/uploads/2020/07/preschool-snacks-in-containers-on-countertop.jpg",
+    drink:
+      "https://www.foodbusinessnews.net/ext/resources/FBN-Features/12/SnackingBeverages_Lead.jpg?height=667&t=1543844419&width=1080",
+  },
   any: "https://icons.veryicon.com/png/o/commerce-shopping/supermarket-goods/food-and-beverage-2.png",
-  drink:
-    "https://www.beveragedaily.com/var/wrbm_gb_food_pharma/storage/images/publications/food-beverage-nutrition/beveragedaily.com/article/2017/12/20/what-s-big-in-beverages-in-2017-innovation-health-and-sustainability/7671501-6-eng-GB/What-s-big-in-beverages-in-2017-Innovation-health-and-sustainability.jpg",
 };
